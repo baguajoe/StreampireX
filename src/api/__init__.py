ac7609258ce2e890d75034679a48c7a7f210a6a7
baguajoe <baguajoe@gmail.com>
2025-07-23 22:28:16 +0000

    src

--- src/api/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-
-# Create a Blueprint instance for your API routes
-api = Blueprint('api', __name__)
-
-# Import your routes *after* the Blueprint is created to avoid circular imports
-from api import routes
