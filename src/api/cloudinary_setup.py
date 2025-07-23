5656b997a70f7177bd55c9f2c7c44aacafa548b6
baguajoe <baguajoe@gmail.com>
2025-07-23 23:09:27 +0000

    src

--- src/api/cloudinary_setup.py
+++ src/api/cloudinary_setup.py
@@ -2,7 +2,8 @@ import cloudinary
 import cloudinary.uploader
 import cloudinary.api
 import os
-from api.cloudinary_setup import cloudinary
+import json
+from cloudinary import CloudinaryImage
 
 cloudinary.config(
     cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
@@ -10,3 +11,39 @@ cloudinary.config(
     api_secret=os.getenv("CLOUDINARY_API_SECRET"),
     secure=True
 )
+
+def uploadFile(file, filename, filetype):
+
+  # Upload the image and get its URL
+  # ==============================
+
+  # Upload the image.
+  # Set the asset's public ID and allow overwriting the asset with new versions
+  cloudinary.uploader.upload("https://cloudinary-devs.github.io/cld-docs-assets/assets/images/butterfly.jpeg", public_id="quickstart_butterfly", unique_filename = False, overwrite=True)
+
+  # Build the URL for the image and save it in the variable 'srcURL'
+  srcURL = CloudinaryImage("quickstart_butterfly").build_url()
+
+  # Log the image URL to the console. 
+  # Copy this URL in a browser tab to generate the image on the fly.
+  print("****2. Upload an image****\nDelivery URL: ", srcURL, "\n")
+
+def getAssetInfo():
+
+  # Get and use details of the image
+  # ==============================
+
+  # Get image details and save it in the variable 'image_info'.
+  image_info=cloudinary.api.resource("quickstart_butterfly")
+  print("****3. Get and use details of the image****\nUpload response:\n", json.dumps(image_info,indent=2), "\n")
+
+  # Assign tags to the uploaded image based on its width. Save the response to the update in the variable 'update_resp'.
+  if image_info["width"]>900:
+    update_resp=cloudinary.api.update("quickstart_butterfly", tags = "large")
+  elif image_info["width"]>500:
+    update_resp=cloudinary.api.update("quickstart_butterfly", tags = "medium")
+  else:
+    update_resp=cloudinary.api.update("quickstart_butterfly", tags = "small")
+
+  # Log the new tag to the console.
+  print("New tag: ", update_resp["tags"], "\n")
