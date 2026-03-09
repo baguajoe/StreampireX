f1b58619a782f46405e99fab71bfa04a3f7b82f1
baguajoe <baguajoe@gmail.com>
2026-03-09 16:08:44 +0000

    Lazy-load R2 clients to fix startup crash + DB migration

--- src/api/jam_tracks_routes.py
+++ src/api/jam_tracks_routes.py
@@ -35,12 +35,10 @@ R2_KEY      = os.environ.get('R2_ACCESS_KEY_ID', '')
 R2_SECRET   = os.environ.get('R2_SECRET_ACCESS_KEY', '')
 R2_BUCKET   = os.environ.get('R2_BUCKET_NAME', 'streampirex-media')
 
-s3 = boto3.client(
-    's3',
-    endpoint_url=R2_ENDPOINT,
-    aws_access_key_id=R2_KEY,
-    aws_secret_access_key=R2_SECRET,
-    config=Config(signature_version='s3v4'),
+# s3 init moved to lazy loader
+def get_s3():
+    import os
+    return boto3.client("s3", endpoint_url=os.environ.get("R2_ENDPOINT"), aws_access_key_id=os.environ.get("R2_ACCESS_KEY_ID"), aws_secret_access_key=os.environ.get("R2_SECRET_ACCESS_KEY"), region_name="auto"),
     region_name='auto',
 )
 
