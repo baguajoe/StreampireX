6b996fe0ea69b91aff766d422c25d96d74521922
baguajoe <baguajoe@gmail.com>
2026-03-11 13:00:21 +0000

    fix: AnnouncementBar.css path, const→let in RecordingStudio

--- src/front/js/component/AnnouncementBar.js
+++ src/front/js/component/AnnouncementBar.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import "../../../styles/AnnouncementBar.css";
+import "../../styles/AnnouncementBar.css";
 
 const AnnouncementBar = () => {
     const [dismissed, setDismissed] = useState(
