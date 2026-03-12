65f5ff9f4841e7454282558c3587484760971058
baguajoe <baguajoe@gmail.com>
2026-03-12 00:49:11 +0000

    Deployment Fix: Validated pages/home path and default component imports

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -1,22 +1,27 @@
 import React, { useContext } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
-import { Home } from "./views/home";
 import injectContext from "./store/appContext";
 import { Context } from "./store/appContext";
-import { Navbar } from "./component/navbar";
-import { Footer } from "./component/footer";
+
+// Corrected path to your pages folder
+import Home from "./pages/home";
+
+// Default imports for components
+import Navbar from "./component/navbar";
+import Footer from "./component/footer";
 import Sidebar from "./component/sidebar";
 
 const Layout = () => {
     const { store } = useContext(Context);
     const basename = process.env.BASENAME || "";
+
     return (
         <div style={{ backgroundColor: "#050a12", minHeight: "100vh", width: "100%" }}>
             <BrowserRouter basename={basename}>
                 <ScrollToTop>
                     <Navbar />
-                    <div className="d-flex">
+                    <div className="d-flex" style={{ backgroundColor: "#050a12" }}>
                         {window.location.search.includes("secret=dev") && <Sidebar user={store.user} />}
                         <div className="flex-grow-1 w-100">
                             <Routes>
