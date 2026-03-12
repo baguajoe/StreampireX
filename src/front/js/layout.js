ffe5fc834466b79ab18aef23931304bc845848cf
baguajoe <baguajoe@gmail.com>
2026-03-12 00:44:34 +0000

    Final UI Fix: Repaired layout structure and removed forced sidebar margins

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -1,16 +1,33 @@
-import React, { useEffect, useContext } from "react";
-import { BrowserRouter as Router } from "react-router-dom";
-import { Context } from "./store/appContext";
-import AppShell from "./component/AppShell";
+import React, { useContext } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ScrollToTop from "./component/scrollToTop";
+import { Home } from "./views/home";
 import injectContext from "./store/appContext";
+import { Context } from "./store/appContext";
+import { Navbar } from "./component/navbar";
+import { Footer } from "./component/footer";
+import Sidebar from "./component/sidebar";
 
 const Layout = () => {
     const { store } = useContext(Context);
+    const basename = process.env.BASENAME || "";
     return (
         <div style={{ backgroundColor: "#050a12", minHeight: "100vh", width: "100%" }}>
-            <Router basename={process.env.BASENAME || ""}>
-                <AppShell user={store.user} />
-            </Router>
+            <BrowserRouter basename={basename}>
+                <ScrollToTop>
+                    <Navbar />
+                    <div className="d-flex">
+                        {window.location.search.includes("secret=dev") && <Sidebar user={store.user} />}
+                        <div className="flex-grow-1 w-100">
+                            <Routes>
+                                <Route element={<Home />} path="/" />
+                                <Route element={<h1>Not found!</h1>} path="*" />
+                            </Routes>
+                        </div>
+                    </div>
+                    <Footer />
+                </ScrollToTop>
+            </BrowserRouter>
         </div>
     );
 };
